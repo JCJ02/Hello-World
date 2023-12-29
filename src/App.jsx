2bee4a07fb559e0140a0c3c047b87d7d0894bc10
JCJ02 <jacobe.johncarlo.02022003@gmail.com>
2023-12-29 09:16:12 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-[#FFFFFF] flex justify-center items-center h-screen w-full">
+      <h1 className="font-dancingScript font-extrabold text-[#262626] text-[25px] md:text-[75px] lg:text-[100px]">Hello World!</h1>
+    </div>
+  )
+}
