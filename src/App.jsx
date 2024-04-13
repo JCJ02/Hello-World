0be06a2ee5b0d8fcb0c87a25a11a60327ae3494d
JCJ02 <jacobe.johncarlo.02022003@gmail.com>
2024-04-14 03:37:31 +0800

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 export default function App() {
   return (
-    <div className="bg-[#FFFFFF] flex justify-center items-center h-screen w-full">
+    <div className="bg-[#FFFFFF] flex flex-col justify-center items-center gap-[100px] h-screen w-full">
       <h1 className="font-dancingScript font-extrabold text-[#262626] text-[25px] md:text-[75px] lg:text-[100px]">Hello World!</h1>
+      <div className="flex flex-col justify-center items-center gap-[10px] text-[#262626]">
+        <label className="font-inter text-[20px]">Implemented by: John Carlo P. Jacobe</label>
+        <label className="font-inter text-[15px]">© Copyright 2024</label>
+      </div>
     </div>
   )
-}
+};
